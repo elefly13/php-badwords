67f7e1f798ca504b9dd34a6f04b29eca0b9327b8
Elefly13 <elymara13@gmail.com>
2021-10-07 17:44:48 +0200

    Esperimenti

--- index.php
+++ index.php
@@ -41,14 +41,28 @@
     Un mondo migliore";
     
     $word = $_GET["word"]; 
+    
 ?>
 
  Stampare a schermo il paragrafo e la sua lunghezza. -->
 <h2><?php echo $text  ?></h2>
 <h2>In questo testo ci sono <?php echo strlen($text) ?> lettere e spazi</h2>
+<!-- str_word_count ritorna il numero di parole  della stringa -->
+<h2>In questo testo ci sono <?php echo $text8 = str_word_count($text) ?> parole</h2>
+
 <!-- Una parola da censurare viene passata dall'utente tramite parametro GET. -->
 <h2><?php echo $text2 = str_replace("Un mondo", $_GET["word"], $text)?></h2>
 <!-- Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 <h2><?php echo $text3 = str_replace($word, "***", $text2)?></h2>
 <h2>Adesso in questo testo ci sono <?php echo strlen($text3) ?> lettere e spazi</h2>
+<h2>Adesso in questo testo ci sono <?php echo $text9 = str_word_count($text3) ?> parole</h2>
+
+
+<!-- Esperimenti -->
+<!-- explode trasforma in array una stringa che separo allo spazio vuoto -->
+<h2><?php echo $text4 = explode(" ", $text) ?></h2>
+<h2><?php echo $text5 = var_dump($text4) ?></h2>
+<!-- str_split trasforma in array una stringa e separa ogni elemento -->
+<h2><?php echo $text6 = str_split($text) ?></h2>
+<h2><?php echo $text7 = var_dump($text6) ?></h2>
 
